8693114c56ca979e24065aeb31f7b4c42fc22dd9
Fadzarsw <fadzar19@gmail.com>
2023-10-18 21:11:05 +0700

    first commit

--- Latihan3.py
+++ Latihan3.py
@@ -7,15 +7,13 @@ print("Variable a =", a)
 print("Variable b =", b)
 
 # Cetak hasil operasi kedua variable dengan string format
+print("Hasil penggabungan {0} & {1} ={2}".format(a,b, a+b))
+
 # Anda harus mengonversi a dan b ke tipe data int sebelum melakukan operasi matematika
 a = int(a)
 b = int(b)
-print("Hasil penggabungan {} & {} = {}".format(a, b, a + b))
+print("Hasil penjumlahan {0} + {1} = {2}".format(a,b, a + b))
 
 # Konversi nilai variable sudah dilakukan di atas, jadi tidak perlu dilakukan lagi
-# Hapus baris berikut
-# a = int(a)
-# b = int(b)
-
 # Perhatikan bahwa operasi pembagian akan menghasilkan float, bukan int
-print("Hasil penjualan {} / {} = {}".format(a, b, a / b))
+print("Hasil penjualan {0} / {1} = {2:.2f}".format(a,b, a / b))
